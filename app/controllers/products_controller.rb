cb366c9e5595b963b31db472c9af525036750118
Eduardo Lago <edulago173@gmail.com>
2026-02-23 17:46:02 -0300

    fix menu navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category, :user).all
+    @products = @products.where(category_id: params[:category_id]) if params[:category_id].present?
   end
 
   def show
