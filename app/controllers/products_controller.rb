69ce134e57b42d01dfa585828b65ad5dfe49947e
Eduardo Lago <edulago173@gmail.com>
2026-03-02 15:49:09 -0300

    feat: add category product page and improve product layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.includes(:category, :user).all
     @products = @products.where(category_id: params[:category_id]) if params[:category_id].present?
+    @selected_category = Category.find_by(id: params[:category_id]) if params[:category_id].present?
   end
 
   def show
+    @product = Product.includes(:category, :user).find(params[:id])
+    @related_products = Product
+      .includes(:category, :user, image_attachment: :blob)
+      .where(category_id: @product.category_id)
+      .where.not(id: @product.id)
+      .limit(9)
   end
 
   def new
